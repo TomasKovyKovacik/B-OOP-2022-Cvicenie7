e43e34a966b5d0a042ee91ffe08841c7189fcdc8
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-30 16:18:39 +0200

    Layouts

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -2,23 +2,43 @@ package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Okno extends UniversalAdapter {
+public class Okno extends JFrame implements ActionListener {
 
     public Okno() throws HeadlessException {
-        super();
+        super("Okno");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400, 400);
 
-        this.addMouseListener(this);
+        this.setLayout(new BorderLayout());
+
+        JPanel menu = new JPanel();
+        menu.setLayout(new GridLayout(1,2));
+        menu.setBackground(Color.CYAN);
+        JLabel label = new JLabel("Tu je text vlavo");
+        JLabel label2 = new JLabel("Tu je text vpravo");
+        menu.add(label);
+        menu.add(label2);
+
+        JPanel klavesnica = new JPanel();
+        klavesnica.setLayout(new GridLayout(3,3));
+
+        for (int i = 1; i < 10; i++) {
+            JButton b = new JButton(String.valueOf(i));
+            klavesnica.add(b);
+            b.addActionListener(this);
+        }
+
+        this.add(menu, BorderLayout.PAGE_START);
+        this.add(klavesnica, BorderLayout.CENTER);
 
         this.setVisible(true);
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println("KLIK : " + e.getPoint().toString());
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
     }
-
 }
